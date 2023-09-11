5cac313bc54847aba509d626e746cf46e42091f5
liuyuting7 <liuyuting7@xiaomi.com>
2023-09-11 17:48:25 +0800

    feat: 每日补偿积分展示部分开发

---
+++ course_config/src/views/pointConfig/constant.js
@@ -0,0 +1,54 @@
+import dayjs from 'dayjs';
+
+export const DateType = {
+  Workday: 1,
+  Weekday: 2
+}
+
+export const ExtraPointLevel = {
+  NoLevel: 0,
+  First: 1,
+  Second: 2,
+  Third: 3
+}
+
+export const ExtraPointLevelLabel = {
+  [ExtraPointLevel.First]: '第一档',
+  [ExtraPointLevel.Second]: '第二档',
+  [ExtraPointLevel.Third]: '第三档'
+}
+
+export const ExtraPointLevelScore = {
+  [ExtraPointLevel.First]: 3,
+  [ExtraPointLevel.Second]: 6,
+  [ExtraPointLevel.Third]: 10
+}
+
+export const DateTypePointStandard = {
+  [DateType.Workday]: {
+    [ExtraPointLevel.First]: 6,
+    [ExtraPointLevel.Second]: 10,
+    [ExtraPointLevel.Third]: 15
+  },
+  [DateType.Weekday]: {
+    [ExtraPointLevel.First]: 12,
+    [ExtraPointLevel.Second]: 16,
+    [ExtraPointLevel.Third]: 20
+  }
+}
+
+function getTableData() {
+  const workdayScoreMap = DateTypePointStandard[DateType.Workday];
+  const weekdayScoreMap = DateTypePointStandard[DateType.Weekday];
+  return Object.keys(ExtraPointLevelLabel).map((level) => {
+    return {
+      level: +level,
+      levelName: ExtraPointLevelLabel[level],
+      workdayScore: workdayScoreMap[level],
+      weekdayScore: weekdayScoreMap[level],
+      pointStandard: ExtraPointLevelScore[level]
+    }
+  })
+}
+
+export const ExtraPointTableData = getTableData();
--- course_config/src/views/pointConfig/list.vue
+++ course_config/src/views/pointConfig/list.vue
@@ -7,6 +7,7 @@
           <h5>（当前积分：{{ totalPoint }}）</h5>
         </div>
         <div style="display: flex; align-items: center;">
+          <el-button round @click="openDialog">获取今日补偿积分</el-button>
           <el-button class="add-btn" type="primary" round @click="handleExchange">兑换活动</el-button>
         </div>
       </el-header>
@@ -16,7 +17,7 @@
           <el-table-column prop="eventName" label="记录内容" min-width="150" />
           <el-table-column prop="score" label="分值" min-width="80">
             <template #default="scope">
-              {{  `${scope.row.score > 0 ? '+' : ''}${scope.row.score}` }}
+              {{ `${scope.row.score > 0 ? '+' : ''}${scope.row.score}` }}
             </template>
           </el-table-column>
           <el-table-column prop="createTime" label="完成时间" min-width="180" :formatter="formatter" />
@@ -29,19 +30,55 @@
       </el-main>
     </el-container>
   </div>
+  <el-dialog v-model="showDialog" modal-class="extra-point-dialog" :title="extraPointInfo.dialogTitle">
+    <div style="margin-top: -23px; padding-top: 18px; padding-left: 10px; border-top: 1px solid #f2f3f5;" />
+    <el-table :data="ExtraPointTableData">
+      <el-table-column property="levelName" label="档数" width="80" />
+      <el-table-column property="workdayScore" label="工作日所需分值" align="center" min-width="150">
+        <template #default="scope">
+          <span :style="getCellStyle(scope.row, DateType.Workday)">{{ getScoreCellText(scope.row, DateType.Workday, scope.row.workdayScore) }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column property="weekdayScore" label="休息日所需分值" align="center" min-width="150">
+        <template #default="scope">
+          <span :style="getCellStyle(scope.row, DateType.Weekday)">{{ getScoreCellText(scope.row, DateType.Weekday, scope.row.weekdayScore)  }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column property="pointStandard" label="补偿分值" align="center" min-width="150">
+        <template #default="scope">
+          <span :style="getCellStyle(scope.row)">{{ scope.row.pointStandard }}</span>
+        </template>
+      </el-table-column>
+    </el-table>
+    <div style="margin-top: 15px;">若每日累计积分达到某一档时，则可获取今日额外补偿积分。各档条件如上</div>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="showDialog = false">{{ extraPointInfo.level === ExtraPointLevel.NoLevel ? '我知道了' : '再等等'
+        }}</el-button>
+        <el-button v-if="extraPointInfo.level !== ExtraPointLevel.NoLevel" type="primary" @click="getExtraPoint">
+          现在获取
+        </el-button>
+      </span>
+    </template>
+  </el-dialog>
 </template>
 
 <script setup>
 import { onMounted, ref } from 'vue';
 import { dayjs } from 'element-plus';
+import ww from 'chinese-workday';
 import { useRouter } from 'vue-router';
 import { getPointList } from './serve';
 import { PointEventTypeLabel } from '../../constant';
+import { ExtraPointTableData, DateType, DateTypePointStandard, ExtraPointLevel } from './constant';
 
 const router = useRouter();
 
+const showDialog = ref(false);
 const totalPoint = ref(0);
 const tableList = ref([]);
+const todayPoint = ref(0);
+const extraPointInfo = ref({});
 
 onMounted(() => {
   getData();
@@ -74,8 +111,70 @@ const formatData = (data) => {
       }
     }
   }
+  todayPoint.value = newDataMap[dayjs().format('YYYY-MM-DD')]?.score ?? 0;
   const newData = Object.keys(newDataMap).map((key) => newDataMap[key]);
   return newData.sort((a, b) => b.id - a.id);
+}
+
+const initExtraPointInfo = () => {
+  const weekNum = dayjs().format('YYYY-MM-DD');
+  const isWorkday = ww.isWorkday(weekNum);
+  const dateType = isWorkday ? DateType.Workday : DateType.Weekday;
+  const scoreMap = DateTypePointStandard[dateType];
+  let dialogTitle = '';
+  let level = ExtraPointLevel.NoLevel;
+  if (todayPoint.value < scoreMap[ExtraPointLevel.First]) {
+    dialogTitle = `当前尚未达到任意档，还需${scoreMap[ExtraPointLevel.First] - todayPoint.value}分，还需继续努力哦。`
+  } else if (todayPoint.value < scoreMap[ExtraPointLevel.Second]) {
+    level = ExtraPointLevel.First;
+    dialogTitle = `当前已满足第${level}档，距离下一档还有${scoreMap[ExtraPointLevel.Second] - todayPoint.value}分，是否现在就获取？（每日仅可获取一次）`;
+  } else if (todayPoint.value < scoreMap[ExtraPointLevel.Third]) {
+    level = ExtraPointLevel.Second;
+    dialogTitle = `当前已满足第${level}档，距离下一档还有${scoreMap[ExtraPointLevel.Third] - todayPoint.value}分，是否现在就获取？（每日仅可获取一次）`;
+  } else {
+    level = ExtraPointLevel.Third;
+    dialogTitle = `当前已满足最高档，再忙也要劳逸结合哦！`;
+  }
+  extraPointInfo.value = {
+    dateType,
+    level,
+    dialogTitle
+  }
+}
+
+const openDialog = () => {
+  showDialog.value = true;
+  initExtraPointInfo();
+}
+
+const getCellStyle = (row, dateType) => {
+  if (dateType && dateType !== extraPointInfo.value.dateType) return '';
+  const { level } = row;
+  if (level === extraPointInfo.value.level) {
+    return 'color: green';
+  }
+  if (level === extraPointInfo.value.level + 1) {
+    return 'color: #FF7034';
+  }
+}
+
+const getScoreCellText = (row, dateType, value) => {
+  if (dateType && dateType !== extraPointInfo.value.dateType) return value;
+  const { level } = row;
+  if (level === extraPointInfo.value.level) {
+    return `${value} (当前达到的档)`;
+  }
+  if (level === extraPointInfo.value.level + 1) {
+    return `${value} (即将达到的档)`;
+  }
+  return value;
+}
+
+const getExtraPoint = async () => {
+  const { dateType, level } = extraPointInfo.value;
+  const scoreMap = DateTypePointStandard[dateType];
+  const score = scoreMap[level];
+  console.log(score);
 }
 
 const formatter = (row) => {
@@ -89,7 +188,7 @@ const handleExchange = () => {
       point: totalPoint.value
     }
   })
-}
+};
 
 </script>
 
@@ -100,6 +199,10 @@ const handleExchange = () => {
   align-items: center;
   margin-top: 5px;
   border-bottom: 1px solid #dcdfe6;
+}
+
+.extra-point-dialog {
+  padding-top: 0;
 }
 
 .common-layout {
